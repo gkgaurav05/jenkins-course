4881b3d9e14c69aae55e8c54e201555150904f22
gkgaurav05 <gkgaurav05@users.noreply.github.com>
2021-06-22 18:33:32 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,15 +1,15 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/gkgaurav05/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('gkumar05')
+            node / gitConfigEmail('gkumar05@outlook.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('NodeJS 8.0.0') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
